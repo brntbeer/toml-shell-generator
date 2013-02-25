126e1bc3bd57f44b2d20b6d66afb39e5bb40ff6e
Brent Beer <brentbeer@github.com>
2013-02-25 13:43:43 +0100

    added 4th argument because of how it should read

--- toml.sh
+++ toml.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+if [-z "$4" ]; then ARG_ERR=ERR; fi
 if [ -z "$3" ]; then ARG_ERR=ERR; fi
 if [ -z "$2" ]; then ARG_ERR=ERR; fi
 if [ -z "$1" ]; then ARG_ERR=ERR; fi
 if [ -n "$ARG_ERR" ];
 then
-    echo "Usage: <filename> <name.subname> <value>"
+    echo "Usage: <filename> <name>.<subname> <value>"
     exit
 fi
 
