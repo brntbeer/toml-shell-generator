c2dcf2ed694a651c8244b04776a019cf501c6416
Brent Beer <brentbeer@github.com>
2013-02-25 14:09:52 +0100

    fixed argument watching syntax
    better readability for how the arguments actually works also

--- toml.sh
+++ toml.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [-z "$4" ]; then ARG_ERR=ERR; fi
+if [ -z "$4" ]; then ARG_ERR=ERR; fi
 if [ -z "$3" ]; then ARG_ERR=ERR; fi
 if [ -z "$2" ]; then ARG_ERR=ERR; fi
 if [ -z "$1" ]; then ARG_ERR=ERR; fi
 if [ -n "$ARG_ERR" ];
 then
-    echo "Usage: <filename> <name>.<subname> <value>"
+    echo "Usage: <filename> <name> <subname> <value>"
     exit
 fi
 
+echo -e "[$2]\n\t$3 = \"$4\"" >> $1
